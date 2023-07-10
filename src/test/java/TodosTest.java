ea83d163847b7c6317f15bee4b0d2ed30c912c34
Viktoria Smirnova <vssmirnova96@gmail.com>
2023-07-10 11:35:30 +0300

    planirovshik

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,171 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldASearchSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches("Позвонить родителям");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noMatchesSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+
+        boolean expected = false;
+        boolean actual = simpleTask.matches("Позвонить друзьям");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesMeetingTopic() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Выкатка 3й версии приложения");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void NoMatchesMeetingTopic() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+
+        boolean expected = false;
+        boolean actual = meeting.matches("Выкатка 5й версии приложения");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesMeetingProject() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+
+        boolean expected = true;
+        boolean actual = meeting.matches("Приложение НетоБанка");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noMatchesMeetingProject() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+
+        boolean expected = false;
+        boolean actual = meeting.matches("Приложение ВТБ Банка");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesEpicMilk() {
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+
+        boolean expected = true;
+        boolean actual = epic.matches("Молоко");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesEpicEggs() {
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+
+        boolean expected = true;
+        boolean actual = epic.matches("Яйца");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesEpicBread() {
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+
+        boolean expected = true;
+        boolean actual = epic.matches("Хлеб");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noMatchesEpic() {
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+
+        boolean expected = false;
+        boolean actual = epic.matches("Батон");
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
